77e42d3c23c46fee9c8f68a79e6352d977306203
CyberL1 <mateusz.hf18@gmail.com>
2024-10-14 21:36:29 +0200

    feat: cache refreshing

--- events/ready.js
+++ events/ready.js
@@ -7,5 +7,25 @@ export default {
     console.log(`Total emojis: ${client.application.emojis.cache.size}`);
 
     console.log(`${client.user.username} ready`);
+
+    setInterval(async () => {
+      console.log("Fetching application emojis after 60 seconds");
+      const oldCacheSize = client.application.emojis.cache.size;
+      const newEmojis = await client.application.emojis.fetch();
+
+      if (newEmojis.size === oldCacheSize) {
+        console.log("Nothing changed");
+      } else if (newEmojis.size > oldCacheSize) {
+        console.log(`New emojis added: ${newEmojis.size - oldCacheSize}`);
+      } else {
+        console.log(`Emojis removed: ${oldCacheSize - newEmojis.size}`);
+
+        // Manually clear and refetch cache
+        client.application.emojis.cache.clear();
+        await client.application.emojis.fetch();
+      }
+
+      console.log(`Total emojis: ${client.application.emojis.cache.size}`);
+    }, 6000);
   },
 };
